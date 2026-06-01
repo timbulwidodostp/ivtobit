fec4779be54deaba488487b81d7ea5b682f17b6b
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2026-06-02 06:19:39 +0700

    ivtobit (tobit1) With (In) R Software

---
+++ ivtobit/ivtobit.R
@@ -0,0 +1,19 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Simultaneous equation tobit model Use ivtobit (tobit1) With (In) R Software
+install.packages("remotes")
+remotes::install_github("cran/tobit1")
+
+library(tobit1)
+# Estimate Simultaneous equation tobit model Use ivtobit (tobit1) With (In) R Software
+ivtobit = read.csv("https://raw.githubusercontent.com/timbulwidodostp/ivtobit/main/ivtobit/ivtobit.csv",sep = ";")
+model <- ivtobit(formula = hours ~ exper + age | educ + motheduc, data = ivtobit, left = 0, method = "ml")
+model_ <- ivtobit(formula = hours ~ exper + age | educ + motheduc, data = ivtobit, left = 0, method = "2steps")
+summary(model)
+summary(model_)
+# Simultaneous equation tobit model Use ivtobit (tobit1) With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
